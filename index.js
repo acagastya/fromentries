c679241dfd228008071222ed6922f16789bf7f52
Agastya Chandrakant <acagastya@outlook.com>
2020-10-01 23:29:34 +0530

    Fixes #18
    Moving from arrow function to older function definition to support pre-ES6 JavaScript engine.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /*! fromentries. MIT License. Feross Aboukhadijeh <https://feross.org/opensource> */
 module.exports = function fromEntries (iterable) {
-  return [...iterable].reduce((obj, [key, val]) => {
+  return [...iterable].reduce(function (obj, [key, val]) {
     obj[key] = val
     return obj
   }, {})
